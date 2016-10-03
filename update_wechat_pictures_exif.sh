7b67b654b7e52ef0e743a8f16c8c0bcd0f4ece4f
Yann Vaillant <yann@vaillant.im>
2016-10-03 12:27:44 +0200

    Fix trying to update files with a date

--- update_wechat_pictures_exif.sh
+++ update_wechat_pictures_exif.sh
@@ -1,5 +1,8 @@
 for pic in *.jpg ; do
-    date=$(date -d @$( echo $pic | egrep -o '[0-9]{10}' ) +'%Y:%m:%d %k:%M:%S')
-    echo "Updating $pic with date $date"
-    exiv2 -M"set Exif.Image.DateTime Ascii $date" $pic
+    old_date=$(exiv2 -g "Exif.Image.DateTime" $pic)
+    if [ ! "$old_date" ];then
+      date=$(date -d @$( echo $pic | egrep -o '[0-9]{10}' ) +'%Y:%m:%d %k:%M:%S')
+      echo "Updating $pic with date $date"
+      exiv2 -M"set Exif.Image.DateTime Ascii $date" $pic
+    fi
 done
